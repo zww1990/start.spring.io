5d2351bb0d7c5de7a49ad5d5bcc2dd5b9f5f33b6
zww1990 <zww1990@foxmail.com>
2020-11-01 22:46:37 +0800

    1、StartApplication.java 2、application.yml

--- start-site/src/main/java/io/spring/start/site/StartApplication.java
+++ start-site/src/main/java/io/spring/start/site/StartApplication.java
@@ -21,7 +21,7 @@ import java.nio.file.Files;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.spring.initializr.versionresolver.DependencyManagementVersionResolver;
-import io.spring.initializr.web.support.SaganInitializrMetadataUpdateStrategy;
+import io.spring.initializr.web.support.LazyInitializrMetadataUpdateStrategy;
 import io.spring.start.site.project.ProjectDescriptionCustomizerConfiguration;
 import io.spring.start.site.support.CacheableDependencyManagementVersionResolver;
 import io.spring.start.site.web.HomeController;
@@ -67,10 +67,16 @@ public class StartApplication {
 		};
 	}
 
+//	@Bean
+//	public SaganInitializrMetadataUpdateStrategy initializrMetadataUpdateStrategy(
+//			RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
+//		return new SaganInitializrMetadataUpdateStrategy(restTemplateBuilder.build(), objectMapper);
+//	}
+	
 	@Bean
-	public SaganInitializrMetadataUpdateStrategy initializrMetadataUpdateStrategy(
+	public LazyInitializrMetadataUpdateStrategy initializrMetadataUpdateStrategy(
 			RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
-		return new SaganInitializrMetadataUpdateStrategy(restTemplateBuilder.build(), objectMapper);
+		return new LazyInitializrMetadataUpdateStrategy(restTemplateBuilder.build(), objectMapper);
 	}
 
 	@Bean
